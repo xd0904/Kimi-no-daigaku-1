74f6a671eb21b923ff123e4e1ffe48647bcf2eb7
xd0904 <83625452+xd0904@users.noreply.github.com>
2025-11-30 00:39:40 +0900

    urls.py 변경

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('highschool-search/', views.highschool_search, name='highschool_search'),
+    path('edurank-search/', views.edurank_search, name='edurank_search'),
 ]
